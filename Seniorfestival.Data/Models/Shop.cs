144e6826d238a008342322c55a0dced212812f70
Martin René Plett Pedersen <martin@plett.dk>
2025-08-13 06:38:21 +0200

    Add location to Shop model

--- Seniorfestival.Data/Models/Shop.cs
+++ Seniorfestival.Data/Models/Shop.cs
@@ -9,6 +9,7 @@ namespace Seniorfestival.Data.Models
         public string? Description { get; set; }
         public string? PictureUrl { get; set; }
         public bool IsShop { get; set; }
+        public string? Location { get; set; }
         public string? Links { get; set; }
 
         public int Id()
